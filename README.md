00b955f37bf81a77836af447eed924395c7f77db
Roman Gurtenko <cpr.korzh@gmail.com>
2024-12-12 05:57:52 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# em_nodejs_history_actions
-This project is designed to test knowledge of Nestjs.
+# History changes service
+This project is designed to test knowledge of Node.js.
+
+## First step
+We're gain compiled project and then starting it.
+Run `npm run build`, `npm start` as well.
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+
+dotenv.config({ path: './.env' });
+
+const app = express();
+const port = process.env.LOCAL_PORT;
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+	console.log(`Server starting on ${port} port`);
+});
